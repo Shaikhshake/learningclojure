03bcf5c1969aacf8860404cbce4493e420710090
ashaikh <ammarshaikh1579@gmail.com>
2023-05-21 00:58:45 +0530

    learning loops

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,22 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Wassupppppppp"))
+
+(println "Now i've got this running")
+
+(defn train
+  []
+  (println "choo choo"))
+
+(str "hi " "how are you?")
+
+(if false
+  (+ 1 1)
+  (+ 2 3))
+
+
+(if true
+  (do (println "This is still not very intuitive") "By the Gods!!") 
+  (do (println "perhaps more practice is needed") "What else is there to do?"))
+
+
