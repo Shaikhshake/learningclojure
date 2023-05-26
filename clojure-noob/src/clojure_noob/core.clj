c3760a99f725d03aa7b815c1104e84372272ec1d
ashaikh <ammarshaikh1579@gmail.com>
2023-05-26 23:07:43 +0530

    learned flow control, comments, wrote a prime-lister and destructuring

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -146,14 +146,14 @@ wow;;
 (count [1 1 2 3])
 (count (set [1 1 2 3]))
 (get [1 2 3] 1);
-(conj [1 3 4] 44.4);
+(conj [1 3 4] 44.4); will join at the end
 (get [1 2 33] 4 "nothing"); I've provided a default value
 (get [1 :c "pubg"] :D "not found")
 ;;can make a vector like this
 (vector "hi" {:a "see"});;
 (vector '(1 2 3));; converts into a vector of single element being [(1 2 3)]
 ;; ?? what is all this?
-(into [] (set {:a :b}) )
+(into [] (set {:a :b}))
 (into [1] (list [1 2]))
 
 
@@ -165,7 +165,7 @@ wow;;
 (list 1 2 3 4);
 (get (nth '(1 {:a "string" :b {:c "madness"}}) 1) :b);;
 (nth '(1 3 4) 3 "3rd index doesn't exist")
-(conj '(2 44 "string") 888)
+(conj '(2 44 "string") 888);; will add at the start
 (list [1 2 3 4]);; convert it into a list of single element benig ([1 2 3 4])
 
 
@@ -294,30 +294,12 @@ wow;;
 
 (mix-match "hi" "how " " are you?" "Okay")
 
-;; Destructuring vectors
-(defn vec-destructure
-  [[first-thang second-thang & remainder-thangs]]
-  (println (str "here the first and second thing of the vector: " first-thang " " second-thang))
-  (println (str "and here the rest of em " (clojure.string/join remainder-thangs))))
-
-(vec-destructure [1 2 3 4])
 
 
-;; Destructuring maps
-(defn map-destructure
-  "Lines below say bind one to the value of key :first and bind two to the value of the key :second"
-  [{one :first two :second}]
-  (println (str "first value is " one " and second value is " two)))
-
-(map-destructure {:a 1 :second 2 :first "popeye"})
 
 
-(defn map-destructure-with-modifications
-  "Note that the words associated with the keys must exactly match the keys of map being passed"
-  [{:keys [first second third]}]
-  (println (str "first thing " first " second thing " second " third thing " third)))
 
-(map-destructure-with-modifications {:third 1 :second 2 :first "popeye"})
+
 
 
 ;; ----------------------------------------------------------
@@ -328,7 +310,7 @@ wow;;
 
 (map (fn [value] (println (str value " bing!"))) ["Rahul" "Kshitiz" "Bing" "Bong"])
 ;; can also bind it to a name
-(def what-a-lang (fn [name] (println (str "hope its been a nice day Mr/Miss " name)) ))
+(def what-a-lang (fn [name] (println (str "hope its been a nice day Mr/Miss " name))))
 (what-a-lang "Shaikh")
 
 ;; take the following function
@@ -348,7 +330,7 @@ wow;;
 (map incby10 [10 20 22 2.22 2222])
 
 ;; you can also pass multiple values to an anonymous function
-(#(println(str "hi " %1 " bye " %2)) "Shojo" "Shoyo")
+(#(println (str "hi " %1 " bye " %2)) "Shojo" "Shoyo")
 
 (defn make-greeter
   "This function creates greeter function based on your liking"
@@ -364,15 +346,16 @@ wow;;
 ;; ----------------------------------------------------------
 (let [x 2] x) ;; value of let for is the last thing they evaluate
 (let [x 4] (println x) 5);; this will demonstrate that x in this scope is 4 but the value of let form is 5
+(let [x 2 y 4] (str "x : " x ", y: " y))
 
 
 ;; ----------------------------------------------------------
 ;;loops
 (loop [iteration 0]
-  (println (str "Iteration " iteration ))
-  (if (> iteration 4 )
-   (println "goodday")
-   (recur (inc iteration))))
+  (println (str "Iteration " iteration))
+  (if (> iteration 4)
+    (println "goodday")
+    (recur (inc iteration))))
 
 (defn my-loop
   "This function loops through four times, printing Value of iteration each time
@@ -386,7 +369,59 @@ wow;;
      (my-loop (inc iteration)))))
 
 (my-loop)
+(var my-loop)
 
+(defn increasing-stars
+  [num-of-lines]
+  (loop [star-line "" n num-of-lines]
+    (if (<= n 0)
+      "Thanks for Playing"
+      (do (println star-line)
+          (recur (str star-line "*") (dec n))))))
+(increasing-stars 3)
+
+;; prints all primes till the given number
+(defn list-all-primes-till
+  [num]
+  (loop [i 2]
+    (loop [j 2]
+      (if (= i j) (println j))
+      (if (and (< j i) (not= (rem i j) 0))
+        (recur (inc j))))
+    (if (< i num)
+      (recur (inc i)))))
+
+(list-all-primes-till 14);; Took me 2 hours to figure this out Dammnnnn
+
+
+;; different way of checking for primes using sequences
+(defn new-prime-checker
+  [teee]
+  (every? false? (map #(= (rem teee %) 0) (range 2 teee))))
+
+(defn refactored-prime-checker
+  [num]
+  (if (= num 1)
+    false)
+  (->> (range 2 num)
+       (map #(= (rem num %)))
+       (every? false?)))
+
+(refactored-prime-checker 1)
+
+
+
+;; ----------------------------------------------------------
+;; For loops
+;; in short, Clojure has no for loops and no direct mutable variables
+(defn give-back-indexed 
+  [coll]
+  (map-indexed vector coll))
+(give-back-indexed {:a 1 :b 2}); ([0 [:a 1]] [1 [:b 2]])
+(give-back-indexed '(1 2 3 3 4));([0 1] [1 2] [2 3] [3 3] [4 4])
+(give-back-indexed "now is not a good time")
+
+;; MUST COME BACK AND REVISIT FOR LOOPS in CHAPTER 2
 
 ;; ----------------------------------------------------------
 ;; regex
@@ -399,3 +434,96 @@ wow;;
 (+ 1 0.0000000000001M); add a M at the end
 (* 1000N 10000 10000 1000 10000000000)
 
+;;-----------------------------------------------------------
+;; Vars - Are Bound To Names
+;; object defined using defn or def are stored in a Clojure var, 
+;; can be accessed using (var a-symbol) or more likely #'a-symbol
+(def ten 10);
+(var ten); #'clojure-noob.core/ten
+#'ten ;; same as above #'clojure-noob.core/ten
+;;note that the value of associated with ten (10) is different from its var
+
+
+;; -----------------------------------------------------------
+;; Bindings, note that vars are bound to names, but there
+;; are other kinds of bindings another example is let binding
+;; that you're already aware of.
+
+;; -----------------------------------------------------------
+
+;; Destructuring 
+
+;; destructuring maps
+(defn map-destructure
+  "Lines below say bind one to the value of key :first and bind two to the value of the key :second"
+  [{one :first two :second}]
+  (println (str "first value is " one " and second value is " two)))
+
+(map-destructure {:a 1 :second 2 :first "popeye"})
+
+
+(defn map-destructure-with-modifications
+  "Note that the words associated with the keys must exactly match the keys of map being passed"
+  [{:keys [first second third]}]
+  (println (str "first thing " first " second thing " second " third thing " third)))
+
+(map-destructure-with-modifications {:third 1 :second 2 :first "popeye"})
+
+
+;; Destructuring vectors
+(defn vec-destructure
+  [[first-thang second-thang & remainder-thangs]]
+  (println (str "here the first and second thing of the vector: " first-thang " " second-thang))
+  (println (str "and here the rest of em " (clojure.string/join remainder-thangs))))
+
+(vec-destructure [1 2 3 4])
+(vec-destructure [1])
+
+
+;; it is also possible to bind both a collection and elements within it 
+;; simultaneously
+(let [[x y :as test] [1 2 3 4 5 6]] (str "x " x " y: " y " test: " (str  test)))
+
+;; example showing how to skip elements form the start
+;; suppose you only want to bind the third element
+;; note that the underscore is assigned multiple times
+(let [[_ _ _ k] [1 2 3 4]] (str "k: " k ", _ : " _))
+
+(def testsymbol 10)
+(resolve 'testsymbol)
+#'testsymbol
+
+;; -------------------------------------------------------------------
+;; MUST STUDY NAMESPACES
+;; https://clojure.org/reference/namespaces 
+;; LOOK INTO THIS WHEN YOU GOT THE TIME OR THE ENERGY
+
+;; -------------------------------------------------------------------
+;; METADATA - data that is orthogonal to the logical value of an object
+(meta #'str)
+
+(meta #'+)
+(meta #'+')
+(meta #'str)
+
+(var +)
+#'+
+(meta (var +'))
+;; don't need to know how to add metadata yet, will come back if needed
+
+;; -------------------------------------------------------------------
+;; can Call java from clojure. Not really interested in using this.
+;; just remember that you can call javadocs in clojure to 
+;; find what something does in java, Example
+
+(javadoc java.util.Scanner);; will take you to the doc page for Scanner
+
+;; -------------------------------------------------------------------
+;; Comments- anything after ; is ignored
+;; can also have multiline comments using
+(comment
+  (def ignore-me 10))
+;; the above code is ignored but it is read by the Clojure reader and
+;; therefore it must be correct
+
+;; -------------------------------------------------------------------
